7c0dcc12bb2886b4aa44330da30a8cb6df1728b6
Vinay Kumar Thota <vinay.thota@internal.salesforce.com>
2019-09-21 21:58:24 +0530

    logger and streaming tests added

---
+++ logger-example/src/main/java/com/test/Test.java
@@ -0,0 +1,13 @@
+package com.test;
+
+
+public class Test {
+	
+	public static String getDataToLog() throws InterruptedException {
+		
+		Thread.sleep(5000);
+		
+		return "Test Message from Java Class";
+	}
+
+}
